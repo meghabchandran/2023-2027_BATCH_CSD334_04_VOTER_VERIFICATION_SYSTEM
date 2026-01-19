867f9d9bce113b83e3a90412c1ce2c5b8821bc12
akshajabiju12 <akshajabiju12@gmail.com>
2026-01-19 15:43:18 +0530

    Vite app setup with routing and page shells

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Home from "./pages/Home"
+import Search from "./pages/Search"
+import Verify from "./pages/Verify"
+import Result from "./pages/Result"
+
 function App() {
-  return <h1>App Skeleton Ready</h1>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="/verify" element={<Verify />} />
+        <Route path="/result" element={<Result />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
