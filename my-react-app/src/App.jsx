b313c132c2c838c5ea82300fb0f9b0785f1f4580
akshajabiju12 <akshajabiju12@gmail.com>
2026-02-08 00:22:39 +0530

    Add login and dashboard pages with routes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -3,6 +3,8 @@ import Home from "./pages/Home"
 import Search from "./pages/Search"
 import Verify from "./pages/Verify"
 import Result from "./pages/Result"
+import Login from "./pages/Login"
+import Dashboard from "./pages/Dashboard"
 
 function App() {
   return (
@@ -12,6 +14,8 @@ function App() {
         <Route path="/search" element={<Search />} />
         <Route path="/verify" element={<Verify />} />
         <Route path="/result" element={<Result />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
   )
