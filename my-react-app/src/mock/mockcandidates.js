9577ca1cd55c2172478b8a8a0333afc3de319970
Malavika S <168813589+Malavika-5@users.noreply.github.com>
2026-02-07 13:03:36 +0530

    added placeholder images

--- my-react-app/src/mock/mockcandidates.js
+++ my-react-app/src/mock/mockcandidates.js
@@ -3,22 +3,22 @@ const mockCandidates = [
     id: 1,
     name: "Anil Kumar",
     party: "People’s Democratic Party",
-    photo: "https://via.placeholder.com/80",
-    partyLogo: "https://via.placeholder.com/40",
+    photo: "./image/cand1.jpg",
+    partyLogo: "./image/logo1.jpg",
   },
   {
     id: 2,
     name: "Suma Reddy",
     party: "National Reform Party",
-    photo: "https://via.placeholder.com/80",
-    partyLogo: "https://via.placeholder.com/40",
+    photo: "./image/cand2.jpg",
+    partyLogo: "./image/logo2.jpg",
   },
   {
     id: 3,
     name: "Rahul Verma",
     party: "United Citizens Front",
-    photo: "https://via.placeholder.com/80",
-    partyLogo: "https://via.placeholder.com/40",
+    photo: "./image/cand3.jpg",
+    partyLogo: "./image/logo3.jpg",
   },
 ];
 
